9ed06e476b897038ef3f61da79c0ce5e7a77191a
Rafał Zaborowski <raffik2000@gmail.com>
2021-04-13 23:57:16 +0200

    Add Datebase

---
+++ RestaurantAPI/Entities/RestaurantDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Entities
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
---
+++ RestaurantAPI/TemperatureRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantAPI.Controllers
+{
+    public class TemperatureRequest
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+    }
+}
