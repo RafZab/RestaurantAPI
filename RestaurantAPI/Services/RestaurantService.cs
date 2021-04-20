05b4059b484a36c6f3735ec5f919baa837ccadca
Rafał Zaborowski <raffik2000@gmail.com>
2021-04-20 18:31:54 +0200

    Controller refactoring

---
+++ RestaurantAPI/Services/RestaurantService.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Entities;
+using RestaurantAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestaurantAPI.Services
+{
+    public interface IRestaurantService
+    {
+        int Create(CreateRestaurantDto dto);
+        IEnumerable<RestaurantDto> GetAll();
+        RestaurantDto GetById(int id);
+    }
+
+    public class RestaurantService : IRestaurantService
+    {
+        private readonly RestaurantDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public RestaurantService(RestaurantDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public int Create(CreateRestaurantDto dto)
+        {
+            var restaurant = _mapper.Map<Restaurant>(dto);
+            _dbContext.Restaurants.Add(restaurant);
+            _dbContext.SaveChanges();
+
+            return restaurant.Id;
+        }
+
+        public RestaurantDto GetById(int id)
+        {
+            var restaurant = _dbContext.Restaurants
+                        .Include(r => r.Address)
+                        .Include(r => r.Dishes)
+                        .FirstOrDefault(r => r.Id == id);
+
+            if (restaurant is null)
+                return null;
+
+            var result = _mapper.Map<RestaurantDto>(restaurant);
+            return result;
+        }
+
+        public IEnumerable<RestaurantDto> GetAll()
+        {
+            var restaurant = _dbContext.Restaurants
+                        .Include(r => r.Address)
+                        .Include(r => r.Dishes)
+                        .ToList();
+
+            var result = _mapper.Map<IEnumerable<RestaurantDto>>(restaurant);
+            return result;
+        }
+    }
+}
