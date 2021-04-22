1eab63241caf85fa20bf8db4b01ee94172d40462
Rafał Zaborowski <raffik2000@gmail.com>
2021-04-22 20:58:49 +0200

    Adding Swagger

--- RestaurantAPI/Startup.cs
+++ RestaurantAPI/Startup.cs
@@ -34,6 +34,7 @@ namespace RestaurantAPI
             services.AddAutoMapper(this.GetType().Assembly);
             services.AddScoped<RestaurantService>();
             services.AddScoped<ErrorHandlingMiddleware>();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +48,12 @@ namespace RestaurantAPI
             app.UseErrorHandlingMiddleware();
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Restaurant API");
+            });
 
             app.UseRouting();
 
